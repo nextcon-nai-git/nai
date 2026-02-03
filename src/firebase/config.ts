09fb7cb6264120b2fc82aee8043894621f6407af
Nextcon Saúde <nextcon@nextconsaude.com.br>
2026-02-03 11:44:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8439299034-125c7",
+  "appId": "1:1061319966767:web:24cc91d57cf4c831d686da",
+  "apiKey": "AIzaSyB8PfD-uf4PPceogHMCeW6IsOPALk6aLxo",
+  "authDomain": "studio-8439299034-125c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1061319966767"
+};
