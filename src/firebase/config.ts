5e958e9e31c23dc7642560eef00ed662873d85f0
Nextcon Saúde <nextcon@nextconsaude.com.br>
2026-02-06 20:40:31 +0000

    Firebase Storage: No default bucket found. Did you set the 'storageBucke

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1061319966767:web:24cc91d57cf4c831d686da",
   "apiKey": "AIzaSyB8PfD-uf4PPceogHMCeW6IsOPALk6aLxo",
   "authDomain": "studio-8439299034-125c7.firebaseapp.com",
+  "storageBucket": "studio-8439299034-125c7.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "1061319966767"
 };
