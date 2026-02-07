ab04aac569fcc4518533df76dff6e3da7923df90
Nextcon Saúde <nextcon@nextconsaude.com.br>
2026-02-07 02:36:36 +0000

    deixe apenas um cliente Time Now, está duplicado

--- src/lib/real-data.ts
+++ src/lib/real-data.ts
@@ -4,15 +4,44 @@
  */
 
 export const REAL_COMPANIES = [
-  // CONTRATOS MASTER (PAIS)
-  { id: "CLI_TIMENOW", name: "TIMENOW GESTÃO DE OBRAS LTDA", city: "Vitória", state: "ES", cnpj: "48.865.462/0001-06", segment: "CONSTRUCTION", isParent: true },
+  // CONTRATO MASTER ÚNICO (PAI)
+  { 
+    id: "CLI_TIMENOW", 
+    name: "TIMENOW GESTÃO DE OBRAS LTDA", 
+    city: "Vitória", 
+    state: "ES", 
+    cnpj: "48.865.462/0001-06", 
+    segment: "CONSTRUCTION", 
+    isParent: true 
+  },
   
-  // SUBUNIDADES TIME NOW (FILHOS)
-  { id: "TN_SOLENIS_IGARASSU", name: "SOLENIS - IGARASSU PE", city: "Igarassu", state: "PE", segment: "INDUSTRY", parentId: "CLI_TIMENOW", address: "ROD PE-041, S/N - KM 6.5, ARARIPE CEP: 53.659-899 - IGARASSU – PE" },
-  { id: "TN_AM_VEGA", name: "ARCELORMITTAL VEGA", city: "São Francisco do Sul", state: "SC", segment: "INDUSTRY", parentId: "CLI_TIMENOW", address: "Rodovia Duque de Caxias, s/n - Iperoba, São Francisco do Sul - SC" },
-  { id: "TN_NESTLE_CACAPAVA", name: "NESTLE CAÇAPAVA", city: "Caçapava", state: "SP", segment: "INDUSTRY", parentId: "CLI_TIMENOW", address: "Av. Henry Nestle, 1800 - Vila Galvão, Caçapava - SP" },
-  { id: "TN_SUZANO_TL", name: "SUZANO TRÊS LAGOAS", city: "Três Lagoas", state: "MS", segment: "INDUSTRY", parentId: "CLI_TIMENOW", address: "ROD BR 158 KM 298, S/N - Três Lagoas - MS" },
-  { id: "TN_VALE_SALOBO", name: "VALE - MINA DO SALOBO", city: "Marabá", state: "PA", segment: "INDUSTRY", parentId: "CLI_TIMENOW", address: "Estrada Mina do Salobo, s/n - Marabá - PA" },
+  // SUBUNIDADES TIME NOW (FILHOS) - VINCULADAS AO ID ACIMA
+  { id: "TN_SUZANO_TL", name: "SUZANO TRÊS LAGOAS", city: "Três Lagoas", state: "MS", parentId: "CLI_TIMENOW", address: "ROD BR 158 KM 298, S/N - Três Lagoas - MS" },
+  { id: "TN_JACAREI", name: "JACAREÍ - SP", city: "Jacareí", state: "SP", parentId: "CLI_TIMENOW", address: "Rod. Gen. Euryale de Jesus Zerbine, Km 84" },
+  { id: "TN_SUZANO_CERRADO", name: "SUZANO CERRADO", city: "Ribas do Rio Pardo", state: "MS", parentId: "CLI_TIMENOW", address: "Rodovia BR margem direita do Km 617" },
+  { id: "TN_VALE_SALOBO_15", name: "VALE PARADAS GPPI SALOBO - 15", city: "Marabá", state: "PA", parentId: "CLI_TIMENOW", address: "Acampamento 3 Alfa – Floresta Nacional Tapirape, Aquiri" },
+  { id: "TN_VALE_PS_13", name: "VALE 13 - POWER SHIFT", city: "Vitória", state: "ES", parentId: "CLI_TIMENOW", address: "Avenida Dante Michelini, No 5500 - Parque Industrial" },
+  { id: "TN_VALE_IP_14", name: "VALE INSPEÇÃO PREDITIVA - 14", city: "Vitória", state: "ES", parentId: "CLI_TIMENOW", address: "Avenida Dante Michelini, No 5500 - Parque Industrial" },
+  { id: "TN_NESTLE_CACAPAVA", name: "NESTLE CAÇAPAVA - SP", city: "Caçapava", state: "SP", parentId: "CLI_TIMENOW", address: "AV: Henry Nestle, 1800 - Vila Galvão" },
+  { id: "TN_VALE_BRIX_16", name: "VALE 16 - BRIQUETAGEM VIX", city: "Vitória", state: "ES", parentId: "CLI_TIMENOW", address: "Avenida Dante Michelini, No 5500 - Parque Industrial" },
+  { id: "TN_VPORTS_01", name: "VPORTS 01", city: "Vitória", state: "ES", parentId: "CLI_TIMENOW", address: "AV GETULIO VARGAS, 556 – CENTRO – VITORIA/ ES" },
+  { id: "TN_SUZANO_ARACRUZ", name: "SUZANO - ARACRUZ - ES", city: "Aracruz", state: "ES", parentId: "CLI_TIMENOW", address: "Rod. Aracruz x Barra do Riacho S/N Km 25" },
+  { id: "TN_BAYER_CAMACARI", name: "BAYER 02 CAMAÇARI BA", city: "Camaçari", state: "BA", parentId: "CLI_TIMENOW", address: "Rua Eterno, no 5001 - Pólo Petroquímico" },
+  { id: "TN_SUZANO_TM_18", name: "SUZANO 18 - Impl.Terminal Modal", city: "Inocência", state: "MS", parentId: "CLI_TIMENOW", address: "Rod MS 316 sentido Paranaíba a Inocência Km 66" },
+  { id: "TN_SUZANO_19", name: "SUZANO 19", city: "Santos", state: "SP", parentId: "CLI_TIMENOW", address: "Av. Governador Mário Covas Junior, S/N Terminal 32" },
+  { id: "TN_ALCOA_POCOS", name: "ALCOA - POÇOS DE CALDAS", city: "Poços de Caldas", state: "MG", parentId: "CLI_TIMENOW", address: "ROD POCOS DE CALDAS - ANDRADAS KM 10, S/N" },
+  { id: "TN_ALCOA_JURUTI", name: "ALCOA - UNIDADE JURUTI - PA", city: "Juruti", state: "PA", parentId: "CLI_TIMENOW", address: "LG ENSEADA DO LAGO GRANDE DE JURUTI, S/N" },
+  { id: "TN_BUNGE_MT", name: "Bunge - Implantação Caldeira", city: "Rondonópolis", state: "MT", parentId: "CLI_TIMENOW", address: "ROD BR 364, SN - PARQUE INDUSTRIAL VETORASSO" },
+  { id: "TN_BUNGE_BA", name: "Bunge - Corporativo GPP", city: "Luis Eduardo Magalhães", state: "BA", parentId: "CLI_TIMENOW", address: "AV DIOCLECIO RAMOS, 1636 - JARDIM PRIMAVERA" },
+  { id: "TN_CONTINENTAL", name: "CONTINENTAL PNEUS", city: "Camaçari", state: "BA", parentId: "CLI_TIMENOW", address: "Rod Ba 530 Via Cetrel Via Atlântica S/N" },
+  { id: "TN_VALE_INSP_PARA", name: "VALE 15 - INSPEÇAO PARA", city: "Marabá", state: "PA", parentId: "CLI_TIMENOW", address: "68501-535, Marabá, PA - Mina do Salobo" },
+  { id: "TN_VALE_FERROVIA", name: "VALE – FERROVIA (PA/MA)", city: "São Luís", state: "MA", parentId: "CLI_TIMENOW", address: "Av dos Portugueses, S/N – ITAQUI/SAO LUIS" },
+  { id: "TN_VALE_RU_SALOBO", name: "Vale 19 - RAMP - UP - SALOBO", city: "Marabá", state: "PA", parentId: "CLI_TIMENOW", address: "Estrada Mina do Salobo, s/n - Pará" },
+  { id: "TN_DOW_QUIMICOS", name: "DOW QUIMICOS (BREU BCO - PA)", city: "Breu Branco", state: "PA", parentId: "CLI_TIMENOW", address: "ROD PA 263 KM 3 5, S/N - INTERIOR" },
+  { id: "TN_DORF_KETAL", name: "DORF KETAL BRASIL LTDA", city: "Nova Santa Rita", state: "RS", parentId: "CLI_TIMENOW", address: "RUA DA PEDREIRA, 559 – PEDREIRA" },
+  { id: "TN_SOLENIS_IGARASSU", name: "SOLENIS - IGARASSU PE", city: "Igarassu", state: "PE", parentId: "CLI_TIMENOW", address: "ROD PE-041, S/N - KM 6.5, ARARIPE" },
+  { id: "TN_AM_VEGA", name: "ARCELORMITTAL VEGA", city: "São Francisco do Sul", state: "SC", parentId: "CLI_TIMENOW", address: "Rodovia Duque de Caxias, s/n - Iperoba" },
+  { id: "TN_AM_PECEM", name: "Arcelormittal-Pecem-GPP", city: "Pecém", state: "CE", parentId: "CLI_TIMENOW", address: "ROD CE - 085, Pecém - São Gonçalo de Amarante" },
   
   // EMPRESAS GERAIS
   { id: "CLI_CONSTRUFAM", name: "CONSTRUFAM ENGENHARIA", city: "Curitiba", state: "PR", segment: "CONSTRUCTION" },
@@ -21,37 +50,26 @@ export const REAL_COMPANIES = [
   { id: "CLI_MONTEC", name: "MONTEC MONTAGENS", city: "Curitiba", state: "PR", segment: "CONSTRUCTION" },
   { id: "CLI_INCORPORADORA", name: "INCORPORADORA GRAN-PARA", city: "Curitiba", state: "PR", segment: "CONSTRUCTION" },
   { id: "CLI_LLM", name: "LLM TERRAPLENAGEM", city: "Curitiba", state: "PR", segment: "CONSTRUCTION" },
-  { id: "CLI_BIAVATTI", name: "BIAVATTI LTDA", city: "Curitiba", state: "PR", segment: "GENERAL" },
   { id: "CLI_GIROTECH", name: "GIROTECH TECNOLOGIA", city: "Curitiba", state: "PR", segment: "INDUSTRY" },
   { id: "CLI_PRMAX", name: "PRMAX SERVICOS", city: "Curitiba", state: "PR", segment: "GENERAL" },
   { id: "CLI_CDA", name: "CDA STEEL FABRICACAO", city: "Curitiba", state: "PR", segment: "INDUSTRY" },
   { id: "CLI_VERSUS", name: "VERSUS SEGURANCA", city: "Curitiba", state: "PR", segment: "GENERAL" },
   { id: "CLI_PLM", name: "PLM REFORMAS EM GERAL", city: "Curitiba", state: "PR", segment: "CONSTRUCTION" },
   { id: "CLI_ESSENCIAL", name: "ESCOLA ESSENCIAL VIRTUDES", city: "Curitiba", state: "PR", segment: "GENERAL" },
-  { id: "CLI_NXC", name: "NXC SST EMPRESARIAL", city: "Curitiba", state: "PR", segment: "GENERAL" },
+  { id: "CLI_NXC", name: "NextCon SST EMPRESARIAL", city: "Curitiba", state: "PR", segment: "GENERAL" },
   { id: "CLI_NOXI", name: "NOXI QUIMICA", city: "Curitiba", state: "PR", segment: "INDUSTRY" },
   { id: "CLI_CENTRAL_TURBOS", name: "CENTRAL TURBOS PARANA", city: "Curitiba", state: "PR", segment: "INDUSTRY" },
-  { id: "CLI_PRIMOR", name: "PRIMOR SERVICOS", city: "Curitiba", state: "PR", segment: "GENERAL" },
 ];
 
 export const REAL_EMPLOYEES = [
-  // CONSTRUFAM
   { id: "EMP_CON_01", name: "BRUNO GADELHA DA SILVA", companyId: "CLI_CONSTRUFAM", jobRole: "Hidrometrista" },
   { id: "EMP_CON_02", name: "JOÃO BESTEL DE DEUS", companyId: "CLI_CONSTRUFAM", jobRole: "Técnico" },
-  { id: "EMP_CON_03", name: "PAULO HENRIQUE MASTECK", companyId: "CLI_CONSTRUFAM", jobRole: "Operacional" },
-  // GULA
   { id: "EMP_GUL_01", name: "ERICK DE OLIVEIRA HENRIQUE", companyId: "CLI_GULA", jobRole: "Produção" },
   { id: "EMP_GUL_02", name: "ANTONIO ROBERTO PEREIRA", companyId: "CLI_GULA", jobRole: "Logística" },
-  { id: "EMP_GUL_03", name: "SIDNEY GOMES PERCHIS", companyId: "CLI_GULA", jobRole: "Ajudante" },
-  // PROMATEC
   { id: "EMP_PRO_01", name: "LUCIANO GREIN", companyId: "CLI_PROMATEC", jobRole: "Técnico" },
   { id: "EMP_PRO_02", name: "DIEGO RAMOS LEONE", companyId: "CLI_PROMATEC", jobRole: "Manutenção" },
-  // MONTEC
   { id: "EMP_MON_01", name: "FELIPE ANDREACI SUEROZ", companyId: "CLI_MONTEC", jobRole: "Montador" },
   { id: "EMP_MON_02", name: "LEONILSON DE OLIVEIRA", companyId: "CLI_MONTEC", jobRole: "Soldador" },
-  // BIAVATTI
-  { id: "EMP_BIA_01", name: "ANGELICA ALBINA KOVASKI ARAGAO", companyId: "CLI_BIAVATTI", jobRole: "Vendas" },
-  { id: "EMP_BIA_02", name: "CAMILA INES BITHENCOURT DE SOUZA", companyId: "CLI_BIAVATTI", jobRole: "Administrativo" },
 ];
 
 export const REAL_EXAMS_HISTORY = [
@@ -63,12 +81,6 @@ export const REAL_EXAMS_HISTORY = [
   { employeeName: "DIEGO RAMOS LEONE", companyName: "PROMATEC", date: "2026-02-03", type: "PE", provider: "WORKING", aso: "OK", s2220: "pendente", s2240: "pendente" },
   { employeeName: "FELIPE ANDREACI SUEROZ", companyName: "MONTEC", date: "2026-02-03", type: "PE", provider: "COLOMBO", aso: "pendente", s2220: "pendente", s2240: "pendente" },
   { employeeName: "JOCEMAR BUENO", companyName: "INCORPORADORA", date: "2026-02-03", type: "AD", provider: "U DA VIT", aso: "OK", s2220: "pendente", s2240: "pendente" },
-  { employeeName: "LANA FELES ARRUDA", companyName: "PRIMOR", date: "2026-01-29", type: "AD", provider: "WORKING", aso: "OK", s2220: "pendente", s2240: "pendente" },
-  { employeeName: "CRISTIAN PIRES DE LIMA", companyName: "INCORPORADORA", date: "2026-01-23", type: "AD", provider: "N/I", aso: "OK", s2220: "NA", s2240: "NA" },
-  { employeeName: "Brenno Gabriel Gomes Tanelo", companyName: "CENTRAL TURBOS", date: "2026-01-19", type: "AD", provider: "N/I", aso: "OK", s2220: "pendente", s2240: "pendente" },
-  { employeeName: "Helder Leonei Becker de Souza", companyName: "NATIVA", date: "2026-01-19", type: "PE", provider: "N/I", aso: "OK", s2220: "pendente", s2240: "pendente" },
-  { employeeName: "ANGELICA ALBINA KOVASKI ARAGAO", companyName: "BIAVATTI", date: "2026-01-19", type: "PE", provider: "WORKING", aso: "OK", s2220: "pendente", s2240: "pendente" },
-  { employeeName: "MARCELON GOMES DA SILVA", companyName: "NXC", date: "2025-10-06", type: "PE", provider: "SQV", aso: "OK", s2220: "OK", s2240: "OK" },
 ];
 
 export const REAL_EXAMS = [
@@ -80,13 +92,13 @@ export const REAL_EXAMS = [
   { name: "Radiografia de Tórax (Padrão OIT)" },
   { name: "Hemograma Completo" },
   { name: "Glicemia de Jejum" },
-  { name: "Avaliação Psicossocial (Trabalho em Altura / Espaço Confinado)" },
+  { name: "Avaliação Psicossocial" },
   { name: "Acuidade Visual" },
   { name: "Toxicológico (Larga Janela)" },
   { name: "Ácido Hipúrico (Xileno)" },
   { name: "Ácido Metil-Hipúrico (Tolueno)" },
   { name: "Chumbo Sangüíneo" },
-  { name: "Colinesterase Plasmática (Agrotóxicos Organofosforados)" },
+  { name: "Colinesterase Plasmática" },
   { name: "Cádmio Sangüíneo" },
   { name: "Cromo Urinário" },
   { name: "Fenol Urinário (Benzeno)" },
@@ -98,10 +110,10 @@ export const REAL_EXAMS = [
   { name: "Coproporfirina Urinária" },
   { name: "Protoporfirina Zíncica (ZPP)" },
   { name: "Creatinina Urinária" },
-  { name: "Acido Tricloroacetico (Tricloroetileno)" },
+  { name: "Acido Tricloroacetico" },
   { name: "Metanol Urinário" },
-  { name: "Carboxihemoglobina (Monóxido de Carbono)" },
-  { name: "Metahemoglobina (Anilina/Nitrobenzeno)" },
+  { name: "Carboxihemoglobina" },
+  { name: "Metahemoglobina" },
   { name: "Reticulócitos" },
   { name: "Plaquetas" },
   { name: "Gama-GT" },
@@ -112,5 +124,5 @@ export const REAL_EXAMS = [
   { name: "Anti-HCV" },
   { name: "Parasitológico de Fezes (PPF)" },
   { name: "Coprocultura" },
-  { name: "Escarro (Pesquisa de BAAR)" },
+  { name: "Escarro (BAAR)" },
 ];
