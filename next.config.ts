75ae13458071431bdc1a6da82d1447f98a3bd890
Nextcon Saúde <nextcon@nextconsaude.com.br>
2026-02-20 19:12:29 +0000

    Try fixing this error: `Console Error: Invalid src prop (https://storage

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,11 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/**',
+      },
     ],
   },
 };
