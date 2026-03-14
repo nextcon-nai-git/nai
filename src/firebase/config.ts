37365ff326eb27f492ea73d78a942642448585cb
Nextcon Saúde <nextcon@nextconsaude.com.br>
2026-03-14 15:04:59 +0000

    name: Deploy to Firebase
    on:
      push:
        branches:
          - main
          -

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-8439299034-125c7",
-  "appId": "1:1061319966767:web:24cc91d57cf4c831d686da",
-  "apiKey": "AIzaSyB8PfD-uf4PPceogHMCeW6IsOPALk6aLxo",
-  "authDomain": "studio-8439299034-125c7.firebaseapp.com",
-  "storageBucket": "studio-8439299034-125c7.firebasestorage.app",
-  "measurementId": "G-HSDR60NWLX",
-  "messagingSenderId": "1061319966767"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB8PfD-uf4PPceogHMCeW6IsOPALk6aLxo",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-8439299034-125c7.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-8439299034-125c7",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-8439299034-125c7.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1061319966767",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1061319966767:web:24cc91d57cf4c831d686da",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-HSDR60NWLX"
 };
