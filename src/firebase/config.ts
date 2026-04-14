87c7bb68929918dc9b745cef56ca86e8489f4571
Nextcon NAI <nai@nextconsaude.com.br>
2026-04-14 17:39:05 -0300

    chore: update firebase credentials

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,6 +8,6 @@ export const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-8439299034-125c7",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-8439299034-125c7.firebasestorage.app",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1061319966767",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1061319966767:web:24cc91d57cf4c831d686da",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-HSDR60NWLX"
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1061319966767:web:4749907ad26ff517d686da",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-E4BW6VFHZH"
 };
